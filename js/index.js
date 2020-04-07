6b2b90da0f9e96b145cbeb56d6d15514c11be446
Chris Forrest <redrider_cr@yahoo.com>
2020-04-07 19:48:55 -0400

    Chris-Forrest

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// Your code goes here
+
+let h2s = document.querySelectorAll('h2')
+h2s.forEach(h2=>{
+    h2.addEventListener('mouseover', (e) =>{
+        e.target.style.backgroundColor = 'red'
+    })
+})
+
+let logo = document.querySelector('.intro')
+    logo.addEventListener('keydown', (e) =>{
+    e.target.style.backgroundColor = 'purple'
+})
+
+
+
+
+//chngBackgrnClr.addEventListener('mouseover', () => {
+  //  chngBackgrnClr.style.backgroundColor = 'mauve'
+//})
+/*chngBackgrnClr.addEventListener("mouseover", function( event ) {   
+    
+    event.style.color = "mauve"
+})*/
+//debugger
