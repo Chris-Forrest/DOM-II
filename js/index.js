bd2ff8d4ff32cb19ecfa07f82042af1620e05d3c
Chris Forrest <redrider_cr@yahoo.com>
2020-04-08 09:00:15 -0400

    mousover, wheel, dblclick done and working keydown has some problems

--- js/index.js
+++ js/index.js
@@ -1,24 +1,27 @@
+/****************mouseover************* */
 
 let h2s = document.querySelectorAll('h2')
-h2s.forEach(h2=>{
+h2s.forEach(h2   =>  {
     h2.addEventListener('mouseover', (e) =>{
-        e.target.style.backgroundColor = 'red'
+        e.target.style.backgroundColor = 'purple'
     })
 })
+/**********************keydown************ */
 
 let logo = document.querySelector('.intro')
     logo.addEventListener('keydown', (e) =>{
-    e.target.style.backgroundColor = 'purple'
+        e.target.style.backgroundColor = '#4bc3f2'
 })
+/*****************wheel event************** */
 
+logo.addEventListener('wheel', (e) =>{
+    e.target.style.backgroundColor = '#945687'
+}) 
+/*************dragging section***********/
 
+/********************dblclick************* */
 
-
-//chngBackgrnClr.addEventListener('mouseover', () => {
-  //  chngBackgrnClr.style.backgroundColor = 'mauve'
-//})
-/*chngBackgrnClr.addEventListener("mouseover", function( event ) {   
-    
-    event.style.color = "mauve"
-})*/
-//debugger
+let firstSection = document.querySelector('.content-section')
+firstSection.addEventListener('dblclick', (e) => {
+    e.target.style.backgroundColor = '#6ab8fc'
+})
